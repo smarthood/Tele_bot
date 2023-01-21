eb9a7adc48099c134b207372b36cd39d544be1f8
smarthood <renolddickson18@gmail.com>
2023-01-21 11:04:06 +0530

    kedhwj

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def wish():
         current_time = datetime.datetime.now()
         now = current_time.strftime("%H:%M:%S")
         if now == "01:00:10":
-            time.sleep(100)
+            time.sleep(10)
             updater.bot.send_message(chat_id=1313787079,text="Good Morning  ☀️ ")
             updater.bot.send_message(chat_id=1313787079,text=f"Have a {random.choice(stwish)} day!")
             for _ in BDICT:
@@ -53,7 +53,7 @@ def wish():
                     updater.bot.send_message(chat_id=-1001434326296,text=f"Happy {CDICT[i]}  ")
             
 def main():
-    token="5921202727:AAF_c7OE8W_TjJf1r_srYAn0au_Dz8CJrB0"
+    token="5921202727:AAE4ggw1IaX9MP2eNhYcEmcD7YI2D7bVywo"
     today = datetime.datetime.today().strftime("%y-%m-%d")
     global BDICT,CDATE,CYEAR,stwish,CDICT
     stwish=["wonderful","surprise","fantastic","marvellous","good","hopeful","great","nice","special"]
